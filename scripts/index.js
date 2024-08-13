1ea038ca5327bf7f5e58eef5ec73f4c29c750c99
Ingrid <ingriddle125@gmail.com>
2024-08-13 09:03:19 -0400

    attempted to dynamically change position of homepage nodes

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,32 @@
-//trees is 56x56 + 5 padding
-//grass is 28x 28
+//finde the home-page class and add the 3 divs
+// the center point would be the position fo the profile pic
+//position the 3 elements around the center point
+const about_me = document.getElementsByClassName("about-me");
+const projects = document.getElementsByClassName("projects");
+const blog = document.getElementsByClassName("blog");
+const profile_pic = document.getElementsByClassName("profile-pic")[0];
 
-const canvas = document.querySelector(".spritesheet");
 
-const context = canvas.getContext("2d");
 
-const tree_image = new Image();
-tree_image.src = "../img/trees.png";
 
-tree_image.crossOrigin = true;
-const scale = 10;
-const TREE_WIDTH = 56;
-const TREE_HEIGHT = 56;
-const BORDER_WIDTH = 1;
-const SPACING_WIDTH = 4;
-function spritePosition(row, col) {
-  return {
-    x: BORDER_WIDTH + col * (SPACING_WIDTH + TREE_WIDTH),
-    y: BORDER_WIDTH + row * (SPACING_WIDTH + TREE_HEIGHT),
-  };
+const update = ()=>{
+  const rect = profile_pic.getBoundingClientRect();
+  const scale = 1000;
+  const cx = rect.x;
+  const cy = rect.y;
+  console.table({ cx: cx, cy: cy });
+  about_me[0].style.bottom = `${cx-scale}px`;
+  projects[0].style.bottom = `${cx - scale}px`;
+
 }
+update();
+// Add event listener for window resize
+window.addEventListener("resize", update);
 
-let position = spritePosition(1, 0);
-console.table(position);
+// //length of the sides are equal to each other
 
-tree_image.onload = function () {
-  context.drawImage(
-    tree_image,
-    position.x,
-    position.y,
-    TREE_WIDTH,
-    TREE_HEIGHT,
-    0,
-    0,
-    TREE_WIDTH,
-    TREE_HEIGHT
-  );
-};
+//equation would be
 
-
+// (cx, cy)
+// (cos(60)* scale, sin(60)*scale)
+//(cos(60)* -scale, sin(60)*scale)
