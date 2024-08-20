887754fab3d33e8a94f04a9acc369e7ec0f9c911
Ingrid <ingriddle125@gmail.com>
2024-08-19 21:30:09 -0400

    transfering to another device

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
-//finde the home-page class and add the 3 divs
-// the center point would be the position fo the profile pic
-//position the 3 elements around the center point
-const about_me = document.getElementsByClassName("about-me");
-const projects = document.getElementsByClassName("projects");
-const blog = document.getElementsByClassName("blog");
-const profile_pic = document.getElementsByClassName("profile-pic")[0];
+// //finde the home-page class and add the 3 divs
+// // the center point would be the position fo the profile pic
+// //position the 3 elements around the center point
+// const about_me = document.getElementsByClassName("about-me");
+// const projects = document.getElementsByClassName("projects");
+// const blog = document.getElementsByClassName("blog");
+// const profile_pic = document.getElementsByClassName("profile-pic")[0];
 
 
 
 
-const update = ()=>{
-  const rect = profile_pic.getBoundingClientRect();
-  const scale = 1000;
-  const cx = rect.x;
-  const cy = rect.y;
-  console.table({ cx: cx, cy: cy });
-  about_me[0].style.bottom = `${cx-scale}px`;
-  projects[0].style.bottom = `${cx - scale}px`;
+// const update = ()=>{
+//   const rect = profile_pic.getBoundingClientRect();
+//   const scale = 1000;
+//   const cx = rect.x;
+//   const cy = rect.y;
+//   console.table({ cx: cx, cy: cy });
+//   about_me[0].style.bottom = `${cx-scale}px`;
+//   projects[0].style.bottom = `${cx - scale}px`;
 
-}
-update();
-// Add event listener for window resize
-window.addEventListener("resize", update);
+// }
+// update();
+// // Add event listener for window resize
+// window.addEventListener("resize", update);
 
 // //length of the sides are equal to each other
 
